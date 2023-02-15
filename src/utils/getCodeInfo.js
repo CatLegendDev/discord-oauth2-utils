9231453741338f2bdf58761416b0d519f51eec86
Alexsandr1x <zoodogood@gmail.com>
2023-02-15 12:10:22 +0200

    Refact

--- src/utils/getCodeInfo.js
+++ src/utils/getCodeInfo.js
@@ -1,14 +1,24 @@
 const { request } = require('undici');
 
-module.exports = async (data, code) => {
-    const body = `client_id=${data.clientId}&client_secret=${data.clientSecret}&code=${code}&redirect_uri=${data.redirectURI}&grant_type=authorization_code&scope=${ data.scope }`
+module.exports = async ({clientId, clientSecret, redirectURI, scopes}, code) => {
+
+    const GRAND_TYPE = "authorization_code";
+
+    const params = new URLSearchParams({
+        "client_id": clientId,
+        "client_secret": clientSecret,
+        "redirect_uri": redirectURI,
+        "scope": scopes.join(" "),
+        "code": code,
+        "grant_type": GRAND_TYPE
+    }).toString();
     
      const resp = await request('https://discord.com/api/oauth2/token',  {
         method: "POST",
-        body: body,
+        body: params,
         headers: {
 	        'Content-Type': 'application/x-www-form-urlencoded',
-				}
+		}
     });
     const Oauth2Data = await resp.body.json();
     return Oauth2Data;
